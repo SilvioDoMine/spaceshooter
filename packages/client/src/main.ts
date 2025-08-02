d70ab6ccf61d7f93da513aa799f8182fda662271
SilvioDoMine <arthurhenrique@live.com>
2025-08-02 19:47:54 -0300

    Migrando para o main.ts com teste do shared

---
+++ packages/client/src/main.ts
@@ -0,0 +1,7 @@
+import { DEFAULT_GAME_CONFIG, GameConfig, Vector2D } from '@spaceshooter/shared';
+
+console.log('Cliente iniciado');
+console.log('Config do jogo:', DEFAULT_GAME_CONFIG);
+
+const position: Vector2D = { x: 100, y: 200 };
+console.log('Posição inicial:', position);
